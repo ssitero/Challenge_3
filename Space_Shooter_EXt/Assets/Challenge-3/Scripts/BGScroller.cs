ce1f0e4f1f4ef605ef8baae0996334d1af0a6484
Samuel <ssitero@knights.ucf.edu>
2018-11-04 19:03:25 -0500

    .

--- Space_Shooter_EXt/Assets/Challenge-3/Scripts/BGScroller.cs
+++ Space_Shooter_EXt/Assets/Challenge-3/Scripts/BGScroller.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class BGScroller : MonoBehaviour
 {
 	public float scrollSpeed;
-	public float tileSizeZ;
+	
+    public float tileSizeZ;
 
 	private Vector3 startPosition;
 
--- Space_Shooter_EXt/Assets/Challenge-3/Scripts/DestroyByContact.cs
+++ Space_Shooter_EXt/Assets/Challenge-3/Scripts/DestroyByContact.cs
@@ -11,31 +11,31 @@ public class DestroyByContact : MonoBehaviour
 	void Start ()
 	{
 		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
-		if (gameControllerObject != null)
-		{
+		
+        if (gameControllerObject != null){
 			gameController = gameControllerObject.GetComponent <GameController>();
 		}
-		if (gameController == null)
-		{
+		
+        if (gameController == null){
 			Debug.Log ("Cannot find 'GameController' script");
 		}
 	}
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.tag == "Boundary" || other.tag == "Enemy")
-		{
-			return;
+		if (other.tag == "Boundary" || other.tag == "Enemy"){
+			
+            return;
 		}
 
-		if (explosion != null)
-		{
-			Instantiate(explosion, transform.position, transform.rotation);
+		if (explosion != null){
+			
+            Instantiate(explosion, transform.position, transform.rotation);
 		}
 
-		if (other.tag == "Player")
-		{
-			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		if (other.tag == "Player"){
+			
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
 			gameController.GameOver();
 		}
 		
